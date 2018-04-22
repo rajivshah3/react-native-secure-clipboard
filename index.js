7952fbc1433e0c3e4acd4769457f2f43c0a933f3
Rajiv Shah <rajivshah1@icloud.com>
2018-04-22 11:43:37 -0400

    Write JS methods

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-
+/* eslint-disable import/no-unresolved */
 import { NativeModules } from 'react-native';
-
+/* eslint-enable import/no-unresolved */
 const { RNSecureClipboard } = NativeModules;
+
+/**
+ * Copies the content to the clipboard
+ * @param {String} content
+ */
+export const setString = content => {
+  RNSecureClipboard.setString(content);
+};
 
 export default RNSecureClipboard;
