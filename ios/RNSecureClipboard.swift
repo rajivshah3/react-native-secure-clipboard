fbb40d13b979f095a8209461b88834f6a48d393d
Rajiv Shah <rajivshah1@icloud.com>
2018-09-22 19:38:34 -0400

    Disable main queue setup
    Fixes #26

--- ios/RNSecureClipboard.swift
+++ ios/RNSecureClipboard.swift
@@ -12,18 +12,21 @@ import MobileCoreServices
 
 @objc(RNSecureClipboard)
 class RNSecureClipboard: NSObject {
-    
-    @objc func setString(_ content: String) -> Void {
+
+    @objc func setString(_ content: String) {
         if #available(iOS 10.0, *) {
             // Universal Clipboard (along with setItems and localOnly) is only available on iOS 10 and later
             let clipboard = UIPasteboard.general
-            clipboard.setItems([[kUTTypeUTF8PlainText as String: content]], options: [.localOnly : true])
+            clipboard.setItems([[kUTTypeUTF8PlainText as String: content]], options: [.localOnly: true])
         } else {
             // If the device does not support Universal Clipboard, we can store it to the clipboard normally as it won't be available to Handoff anyway
             let clipboard = UIPasteboard.general
             clipboard.string = content
         }
     }
-    
-}
 
+    @objc static func requiresMainQueueSetup() -> Bool {
+        return false
+    }
+
+}
